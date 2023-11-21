be7aa8c9206a9579fbf0035f39bcb12d3029f5d4
Kavinda Botheju <117912732+KJBotheju@users.noreply.github.com>
2023-11-21 21:36:05 +0530

    create StudentView.js

---
+++ src/component/student/StudentsView.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const StudentsView = () => {
+  return (
+    <section>
+      <table>
+        <thead>
+            <tr>
+                <th></th>
+            </tr>
+        </thead>
+      </table>
+    </section>
+  )
+}
+
+export default StudentsView
