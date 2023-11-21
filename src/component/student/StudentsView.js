b0e23a1c6604799b82d26de40179e0f57ed9f08e
Kavinda Botheju <117912732+KJBotheju@users.noreply.github.com>
2023-11-21 22:02:11 +0530

    install axios

--- src/component/student/StudentsView.js
+++ src/component/student/StudentsView.js
@@ -1,14 +1,38 @@
 import React from 'react'
 
 const StudentsView = () => {
+
+    const[students, setStudents] = useState([]); // students is the state variable, setStudents is the function that updates the state variable
   return (
     <section>
       <table>
         <thead>
             <tr>
-                <th></th>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Faculty</th>
+                <th>Actions</th>
             </tr>
         </thead>
+        {students.map((student,index) =>(
+            <tr key={student.id}>
+                <th scope='row'key={index}>
+                    {index + 1}
+                </th>
+                <td>{student.firstName}</td>
+                <td>{student.LastName}</td>
+                <td>{student.email}</td>
+                <td>{student.faculty}</td>            
+                <td>View</td>
+                <td>Update</td>
+                <td>Delete</td>
+            </tr>
+        ))}
+        <tbody>
+            
+        </tbody>
       </table>
     </section>
   )
