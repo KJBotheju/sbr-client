a897364d200b60cb8ef20be3ff835efa4513f9a0
Kavinda Botheju <117912732+KJBotheju@users.noreply.github.com>
2023-11-29 21:19:47 +0530

    add student-profile route to app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NavBar from './component/common/NavBar';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import AddStudent from './component/student/AddStudent';
 import EditStudent from './component/student/EditStudent';
+import StudentProfile from './component/student/StudentProfile';
 
 
 function App() {
@@ -22,6 +23,8 @@ function App() {
           <Route exact path = "/add-student" element = {<AddStudent />}>
           </Route>
           <Route exact path = "/edit-student/:id" element = {<EditStudent />}>
+          </Route>
+          <Route exact path = "/student-profile/:id" element = {<StudentProfile />}>
           </Route>
         </Routes>
       </Router>
